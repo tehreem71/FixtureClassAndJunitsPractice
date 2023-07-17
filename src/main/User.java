c6a0d625fce5fb037e4cfc60c1f5df14bea6d55e
Tehreem Fatima <f190143@cfd.nu.edu.pk>
2023-07-17 13:37:38 +0500

    update according to javadocs and formatting

--- src/main/User.java
+++ src/main/User.java
@@ -2,11 +2,17 @@ package main;
 
 import lombok.*;
 
-public @Data @AllArgsConstructor @NoArgsConstructor @RequiredArgsConstructor class User {
-    private String name;
-    @NonNull
-    private int age;
-    @NonNull
-    private int id;
-
+/**
+ * Represent a User with name, age and ID as attributes.
+ */
+public @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor class User
+{
+  private String name;
+  @NonNull
+  private int age;
+  @NonNull
+  private int id;
 }
--- src/main/UserService.java
+++ src/main/UserService.java
@@ -4,61 +4,83 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * An Implementation of User class that provides CRUD services on User objects
+ * An Implementation of User class that provides CRUD operations for managing User objects
  */
-public class UserService {
-    List<User> users = new ArrayList<User>();
-    /**
-     * Adds a new and valid user to List of Users
-     * @param user an object of User class along with attributes
-     */
-    public User addUser(User user) {
-        if(user.getAge()<0){
-            throw new IllegalArgumentException();
-        }
-        users.add(user);
-        return user;
-    }
-    /**
-     * Deletes a valid user from List of Users
-     * @param user an object of User class along with attributes
-     */
-    public void removeUser(User user) {
-        if(!users.contains(user))
-        {
-            throw new IndexOutOfBoundsException();
-        }
-        users.remove(user);
-    }
-    /**
-     * Edits an existing user and updates its attributes on validity
-     * @param id id of user object passed to the function
-     * @param newName new name to be edited and updated
-     * @param newAge new age to be edited and updated
-     */
-    public void editUser(int id, String newName, int newAge) {
-            try{
-                if(newName == "" || newAge <= 0){
-                    throw new IllegalArgumentException();
-                }
-                User user = users.get(id);
-                user.setName(newName);
-                user.setAge(newAge);
+public class UserService
+{
+  List<User> users = new ArrayList<User>();
 
-            }catch (NullPointerException e){
-                System.out.println("User with ID "+id+" does not exist, or is empty.");
-            }
-                catch (IndexOutOfBoundsException e)
-            {
-                System.out.println("desired user ID is out of Users Array bounds");
-            }
-    }
-    /**
-     * Function to get the complete list of users
-     * @return returns list of users
-     */
-    public List<User> getUsers()
+  /**
+   * Adds a new user to list of Users
+   *
+   * @param user the user object to be added
+   * @throws IllegalArgumentException If the user's age is negative.
+   */
+  public User addUser(User user) throws IllegalArgumentException
+  {
+    if (user.getAge() < 0)
     {
-        return users;
+      throw new IllegalArgumentException();
     }
+    users.add(user);
+    return user;
+  }
+
+  /**
+   * Removes a user from the list of Users
+   *
+   * @param user the user object to be removed.
+   * @throws IndexOutOfBoundsException If the user is not found in the list
+   */
+  public void removeUser(User user) throws IndexOutOfBoundsException
+  {
+    if (!users.contains(user))
+    {
+      throw new IndexOutOfBoundsException();
+    }
+    users.remove(user);
+  }
+
+  /**
+   * Edits attributes of existing user based on ID
+   *
+   * @param id      id of user object to be edited
+   * @param newName new name to be assigned to user object
+   * @param newAge  new age to be assigned to user object
+   * @throws IllegalArgumentException  If the new name is empty or the new age is less than or equal to 0.
+   * @throws NullPointerException      If the user with the given ID does not exist or is null.
+   * @throws IndexOutOfBoundsException If the desired user ID is out of the users array bounds.
+   */
+  public void editUser(int id, String newName, int newAge) throws IllegalArgumentException, NullPointerException, IndexOutOfBoundsException
+  {
+    try
+    {
+      if (newName == "" || newAge <= 0)
+      {
+        throw new IllegalArgumentException();
+      }
+      User user = users.get(id);
+      user.setName(newName);
+      user.setAge(newAge);
+
+    }
+    catch (NullPointerException e)
+    {
+      System.out.println("User with ID " + id + " does not exist, or is empty.");
+    }
+    catch (IndexOutOfBoundsException e)
+    {
+      System.out.println("desired user ID is out of Users Array bounds");
+    }
+  }
+
+  /**
+   * Returns the complete list of users
+   *
+   * @return list of users
+   */
+  public List<User> getUsers()
+  {
+    return users;
+  }
 }
